db3dbf95e1cdfbadc91783fa5c43f43841f71361
jean <jean05997@gmail.com>
2021-07-16 20:38:36 -0400

    Change to the grid
    Instead of making square and then duplicated it to create a grid. NOW we just create lines changing X and then in the Z

--- Comp371Project/src/Grid.h
+++ Comp371Project/src/Grid.h
@@ -6,15 +6,17 @@ class Grid : public Script
 public:
 	void OnRender()
 	{
-		for (int x = -50; x < 50; x++) // started at -50 since it need to be center at origin assuming(0,0) being origin
+		for (int x = -50; x <= 50; x++) // started at -50 since it need to be center at origin assuming(0,0) being origin
 		{
-			for (int z = -50; z < 50; z++)
-			{
-				Renderer3D::DrawWireSquare({ x, -0.5, z }, { 1.5708, 0, 0 }, { 1, 1, 1 });
-				//1.5708rad= 90degree since size of the square is 1 and by rotating it 90 degree it will be at heigh (1,0.5,1) whihc is why the (x,-0.5, z)
-			}
+			Renderer3D::DrawLine(m_transform.GetTransformMatrix(), { x, 0, -50 }, {x, 0, 50 });
 		}
 
+		for (int z = -50; z <= 50; z++)
+		{
+			Renderer3D::DrawLine(m_transform.GetTransformMatrix(), { -50, 0, z }, { 50, 0, z });
+		}
 	}
 
+private:
+		Transform m_transform = Transform({ 0, 0, 0 }, { 0, 0, 0 }, { 1, 1, 1 });
 };
