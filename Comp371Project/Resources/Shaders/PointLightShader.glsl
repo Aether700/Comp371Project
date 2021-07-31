0e449af36b5c08e50897bd450337d6b03004d8ef
Aether700 <hanachian.jamil@gmail.com>
2021-07-31 15:05:29 -0400

    SHADOWS!!!

--- Comp371Project/Resources/Shaders/PointLightShader.glsl
+++ Comp371Project/Resources/Shaders/PointLightShader.glsl
@@ -147,7 +147,7 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     
     /*
     // keep the shadow at 0.0 when outside the far_plane region of the light's frustum.
-    if(projCoords.z > 1.0)aw
+    if(projCoords.z > 1.0)
         shadow = 0.0;
     */   
     return shadow;
