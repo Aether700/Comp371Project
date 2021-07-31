a4ed5579c250ae9dab8870e22c5eea386c411540
jean <jean05997@gmail.com>
2021-07-31 13:53:07 -0400

    toggle for the grid
    added grid to model manager

--- Comp371Project/src/Main.cpp
+++ Comp371Project/src/Main.cpp
@@ -1,5 +1,4 @@
 #define GLEW_STATIC
-#include "Grid.h"
 #include "Axes.h"
 #include "ModelManager.h"
 
@@ -34,7 +33,7 @@ int main()
 	//Add Scripts here   
 
 	Application::AddScript(new Axes());
-	Application::AddScript(new Grid());
+	//Application::AddScript(new Grid());
 	Application::AddScript(new ModelManager());
 	//Application::AddScript(new AlecPointLightTest());
 	//Application::AddScript(new TextureTest());
