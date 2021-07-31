c3d5f335faff00f30deb5e4c16b99da190ad0dd0
madelainemar <madelainemareschal@gmail.com>
2021-07-31 18:07:51 -0400

    idk

--- Comp371Project/Resources/Shaders/PointLightShader.glsl
+++ Comp371Project/Resources/Shaders/PointLightShader.glsl
@@ -145,11 +145,11 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     }
     shadow /= 9.0;
     
-    /*
+    
     // keep the shadow at 0.0 when outside the far_plane region of the light's frustum.
     if(projCoords.z > 1.0)
         shadow = 0.0;
-    */   
+      
     return shadow;
 }
 
